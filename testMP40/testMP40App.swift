ef7a0cfed7df0460b3af5f2bdc2b0cb1d1f382ee
Robert Figueras <rfigueras@ulta.com>
2025-10-13 13:09:56 -0500

    Initial Commit

---
+++ testMP40/testMP40App.swift
@@ -0,0 +1,17 @@
+//
+//  testMP40App.swift
+//  testMP40
+//
+//  Created by Figueras, Robert on 10/13/25.
+//
+
+import SwiftUI
+
+@main
+struct testMP40App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
